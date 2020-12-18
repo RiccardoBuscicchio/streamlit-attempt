813be87c727d11708a30428140666c5192fb75e4
Riccardo Buscicchio <chionatan@gmail.com>
2020-12-18 09:46:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import math
 import pandas as pd
 import streamlit as st
 
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import deepdish as dd
+
 """
 # Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
